9d8e89b86c4675fd80c83c07668af586ab9171d1
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2025-01-19 17:42:29 +0100

    cleanup

--- main.py
+++ main.py
@@ -21,31 +21,8 @@ def clear_dynexite_text(text):
     patterns = [r'\d+ Punkte', r'\d+ Punkt', r'\d+ NKS', r'HEB \d+', r'HEA \d+']  # Subject-specific sequences need to be added manually
     return re.sub('|'.join(patterns), '', text)
 
-def filter_moodle_text(text):
-    lines = text.split('\n')
-    try:
-        start_index = [i for i, line in enumerate(lines) if "Aufgabe" in line and i > 5][0]
-    except IndexError:
-        start_index = [i for i, line in enumerate(lines) if "Antwort:" in line and i > 5][0]
-    end_index = [i for i, line in enumerate(lines) if "Direkt zu:" in line][0]
-
-    lines = lines[start_index:end_index+1]
-    # print(*lines, sep='\n')
-    pattern = r'Antwort:Frage \d+'
-
-    moodle_numbers = []
-    for i, line in enumerate(lines):
-        if re.match(pattern, line):
-            try:
-                moodle_numbers.append(float(lines[i+1].replace(',', '.')))
-            except ValueError:
-                input(f"Error in line {i+1}: {lines[i+1]}")
-    return moodle_numbers
-
-
 def get_leftovers(dynexite_numbers, excel_numbers):
     return list((Counter(excel_numbers) - Counter(dynexite_numbers)).elements())
-
 
 
 def get_wrong_numbers(dynexite_numbers, excel_numbers):
@@ -79,35 +56,31 @@ def yes_no_check(dynexite, excel):
 if __name__ == '__main__':
     tolerance = 0.2
 
-    # if input("Do you want to check Dynexite? (y/n) ") == 'y':
-    # if True:
-    if True:
-        dynexite_text = get_text("Dynexite_Text.txt")
-        excel_text = get_text("Excel_Text.txt")
+    dynexite_text = get_text("Dynexite_Text.txt")
+    excel_text = get_text("Excel_Text.txt")
 
-        dynexite_numbers = filter_numbers(clear_dynexite_text(dynexite_text))
-        excel_numbers = filter_numbers(excel_text)
-        # yes, no = filter_yes_no(dynexite_text)
-        # yes, no = filter_yes_no(excel_text)
+    dynexite_numbers = filter_numbers(clear_dynexite_text(dynexite_text))
+    excel_numbers = filter_numbers(excel_text)
 
-        leftovers = get_leftovers(dynexite_numbers, excel_numbers)
-        wrong_numbers = get_wrong_numbers(dynexite_numbers, excel_numbers)
-        very_close_numbers = get_very_close_numbers(wrong_numbers, leftovers, tolerance)
-        yes_no_check(filter_yes_no(dynexite_text), filter_yes_no(excel_text))
-        if dynexite_numbers == excel_numbers:
-            print("All numbers are correct and in the same order.")
-        elif len(dynexite_numbers) != len(excel_numbers):
-            print(f"Something went wrong. The number of Dynexite numbers {len(dynexite_numbers)} is not equal to the number of Excel numbers {len(excel_numbers)}")
-            print(f"Dynexite numbers: {dynexite_numbers}")
-            print(f"Excel Numbers   : {excel_numbers}")
-        elif len(wrong_numbers) != len(leftovers):
-            print(f"Something went wrong. The number of wrong numbers {len(wrong_numbers)} is not equal to the number of leftovers {len(leftovers)}")
-        elif wrong_numbers:
-            for number in wrong_numbers:
-                print(f"\tNumber {number} is not in Excel. {min(leftovers, key=lambda x: abs(x-number))} is the closest match. Other options: {', '.join(map(str, leftovers))}")
-            if len(very_close_numbers) == len(wrong_numbers):
-                print("All numbers are very close.")
-        else:
-            print("All numbers are correct, but not in the same order:")
-            print(f"Dynexite: {dynexite_numbers}")
-            print(f"Excel:    {excel_numbers}")
+    leftovers = get_leftovers(dynexite_numbers, excel_numbers)
+    wrong_numbers = get_wrong_numbers(dynexite_numbers, excel_numbers)
+    very_close_numbers = get_very_close_numbers(wrong_numbers, leftovers, tolerance)
+    yes_no_check(filter_yes_no(dynexite_text), filter_yes_no(excel_text))
+
+    if dynexite_numbers == excel_numbers:
+        print("All numbers are correct and in the same order.")
+    elif len(dynexite_numbers) != len(excel_numbers):
+        print(f"Something went wrong. The number of Dynexite numbers {len(dynexite_numbers)} is not equal to the number of Excel numbers {len(excel_numbers)}")
+        print(f"Dynexite numbers: {dynexite_numbers}")
+        print(f"Excel Numbers   : {excel_numbers}")
+    elif len(wrong_numbers) != len(leftovers):
+        print(f"Something went wrong. The number of wrong numbers {len(wrong_numbers)} is not equal to the number of leftovers {len(leftovers)}")
+    elif wrong_numbers:
+        for number in wrong_numbers:
+            print(f"\tNumber {number} is not in Excel. {min(leftovers, key=lambda x: abs(x-number))} is the closest match. Other options: {', '.join(map(str, leftovers))}")
+        if len(very_close_numbers) == len(wrong_numbers):
+            print("All numbers are very close.")
+    else:
+        print("All numbers are correct, but not in the same order:")
+        print(f"Dynexite: {dynexite_numbers}")
+        print(f"Excel:    {excel_numbers}")

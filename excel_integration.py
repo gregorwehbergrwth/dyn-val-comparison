fd6ceebdf9807cb7e8e1638e73b709e8aa8cbb63
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2025-04-27 16:40:29 +0200

    fixed excel import issue with zeros

--- excel_integration.py
+++ excel_integration.py
@@ -1,5 +1,5 @@
-
 import openpyxl
+
 
 excel_filepath = r"C:\Users\grego\Downloads\HÜ_01_Excel.xlsx"
 position = 'B13:B36'
@@ -9,7 +9,14 @@ def numbers_from_excel(filepath, location):
     workbook = openpyxl.load_workbook(filepath, data_only=True)
     sheet = workbook.active
 
-    return [number[0].value for number in sheet[location]]
+    numbers = []
+    for cell_tuple in sheet[location]:
+        cell = cell_tuple[0]
+        if cell.value is not None:
+            numbers.append(cell.value)
+        else:
+            numbers.append(0)
+    return numbers
 
 
 def save_numbers_to_file(filename, numbers):

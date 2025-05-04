a0d9972a24d6cb7fbebca96685a1a3d5deae8bac
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2025-05-04 22:21:23 +0200

    fixed excel import issue with floating points, added debug mode

--- clipboard.py
+++ clipboard.py
@@ -2,18 +2,38 @@ import pyperclip
 import time
 
 
-def clipboard_loop(file):
+def clipboard_loop(file, start, sleep):
     with open(file, "r") as file:
         lines = file.readlines()
+
     for i in range(3, 0,-1):
         print(i)
         time.sleep(1)
+
+    go = False if start else True
+
+    for line in lines:
+        if start:
+            if line.strip() == start:
+                go = True
+
+        if line.strip() and go:
+            pyperclip.copy(line.strip())
+            print(f"\"{line.strip()}\" copied to clipboard.")
+            time.sleep(sleep)
+
+def clipboard_debug(file):
+    with open(file, "r") as file:
+        lines = file.readlines()
+
 
     for line in lines:
         if line.strip():
             pyperclip.copy(line.strip())
             print(f"\"{line.strip()}\" copied to clipboard.")
-            time.sleep(2.5)
+            input()
 
 if __name__ == '__main__':
-    clipboard_loop('excel.txt')
+    clipboard_loop('excel.txt', start="", sleep=1.5)
+
+    # clipboard_debug('excel.txt')
